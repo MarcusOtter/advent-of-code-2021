32714f04b2912daf21a189be1408a495e511652e
MarcusOtter <github@otterstrom.dev>
2021-12-02 01:08:50 +0100

    Add second solution first day

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -2,6 +2,10 @@ mod input;
 
 fn main() {
     let inputs = input::get_input();
+    println!("{}", second(inputs));
+}
+
+fn _first(inputs: [i32; 2000]) -> i32 {
     let mut previous = i32::min_value();
     let mut count = -1;
 
@@ -13,5 +17,18 @@ fn main() {
         previous = *num;
     }
 
-	println!("{}", count);
+	return count;
+}
+
+fn second(inputs: [i32; 2000]) -> i32 {
+    let mut count = 0;
+    for i in 0..inputs.len() - 3 {
+        let window_a = inputs[i] + inputs[i + 1] + inputs[i + 2];
+        let window_b = window_a - inputs[i] + inputs[i + 3];
+        if window_b > window_a {
+            count += 1;
+        }
+    }
+
+    return count;
 }
