64db88e5c0343c4d070922c90aa6901b3454cfa4
MarcusOtter <github@otterstrom.dev>
2021-12-02 22:14:14 +0100

    Add 2nd solution day 2

--- d02/src/main.rs
+++ d02/src/main.rs
@@ -9,7 +9,15 @@ fn main() {
 		.lines()
 		.collect::<Vec<&str>>();
 
+	println!("First:");
 	first(commands);
+
+	let commands = input
+		.lines()
+		.collect::<Vec<&str>>();
+
+	println!("\nSecond:");
+	second(commands);
 }
 
 fn first(commands: Vec<&str>) {
@@ -24,6 +32,29 @@ fn first(commands: Vec<&str>) {
 		if      command == "down"    { depth += number; }
 		else if command == "up"      { depth -= number; }
 		else if command == "forward" { position_x += number; }
+	}
+
+	println!("Horizontal position: {}", position_x);
+	println!("Depth: {}", depth);
+	println!("Multiplied: {}", position_x * depth);
+}
+
+fn second(commands: Vec<&str>) {
+	let mut position_x = 0;
+	let mut depth = 0;
+	let mut aim = 0;
+
+	for x in commands {
+		let mut split = x.split_whitespace();
+		let command = split.next().unwrap();
+		let number = split.next().unwrap().parse::<i32>().unwrap();
+		
+		if      command == "down"    { aim += number; }
+		else if command == "up"      { aim -= number; }
+		else if command == "forward" {
+			position_x += number;
+			depth += aim * number;
+		}
 	}
 
 	println!("Horizontal position: {}", position_x);
