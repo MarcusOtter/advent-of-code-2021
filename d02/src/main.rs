720a63521207df0d8f3aa0073e2541d6462c279a
MarcusOtter <github@otterstrom.dev>
2021-12-02 22:01:51 +0100

    Add first solution day 2

--- d02/src/main.rs
+++ d02/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+	let input =
+		read_to_string("input.txt")
+		.expect("Something went wrong reading the input file");
+
+	let commands = input
+		.lines()
+		.collect::<Vec<&str>>();
+
+	first(commands);
+}
+
+fn first(commands: Vec<&str>) {
+	let mut position_x = 0;
+	let mut depth = 0;
+
+	for x in commands {
+		let mut split = x.split_whitespace();
+		let command = split.next().unwrap();
+		let number = split.next().unwrap().parse::<i32>().unwrap();
+		
+		if      command == "down"    { depth += number; }
+		else if command == "up"      { depth -= number; }
+		else if command == "forward" { position_x += number; }
+	}
+
+	println!("Horizontal position: {}", position_x);
+	println!("Depth: {}", depth);
+	println!("Multiplied: {}", position_x * depth);
 }
