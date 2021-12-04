27f2a0cba97f60353e641e40825f0add05c0c431
MarcusOtter <github@otterstrom.dev>
2021-12-04 16:12:16 +0100

    Add part 1 day 3

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -2,19 +2,28 @@ use std::fs::read_to_string;
 
 fn main() {
 	let input =
-		read_to_string("example.txt")
+		read_to_string("input.txt")
 		.expect("Something went wrong reading the input file");
 
 	let binary_numbers = input
 		.lines()
 		.collect::<Vec<&str>>();
 
-	let bits = binary_numbers[0].chars().count();
+	println!("-- First --");
+	first(&binary_numbers);
+
+	println!("\n-- Second --");
+	second(&binary_numbers);
+}
+
+fn first(numbers: &Vec<&str>) {
+	let bits = numbers[0].chars().count();
 
 	let mut ones = vec![0; bits];
 	let mut zeroes = vec![0; bits];
 
-	for num in binary_numbers {
+	// Count zeroes and ones in each position
+	for num in numbers {
 		for i in 0..bits {
 			if num.as_bytes()[i] as char == '1' {
 				ones[i] += 1;
@@ -25,7 +34,29 @@ fn main() {
 		}
 	}
 
-	println!("{:?}", ones);
-	println!("{:?}", zeroes);
+	// I could've probably done this with some bitwise magic but 2s complement logic does not work
+	// well when the amount of bits in an integer are not divisible by 8 and/or unknown
+	let mut gamma_rate_str = "".to_owned();
+	let mut epsilon_rate_str = "".to_owned();
+	for i in 0..ones.len() {
+		if ones[i] > zeroes[i] {
+			gamma_rate_str.push_str("1");
+			epsilon_rate_str.push_str("0");
+		}
+		else {
+			gamma_rate_str.push_str("0");
+			epsilon_rate_str.push_str("1");
+		}
+	}
+
+	let gamma_rate   = u64::from_str_radix(&gamma_rate_str, 2).unwrap();
+	let epsilon_rate = u64::from_str_radix(&epsilon_rate_str, 2).unwrap();
+
+	println!("Gamma: {}\nEpsilon: {}", gamma_rate, epsilon_rate);
+	println!("Power consumption: {}", gamma_rate * epsilon_rate);
+}
+
+fn second(numbers: &Vec<&str>) {
+
 }
 
